7a0e378ebb63e4c281b3eb1e362b47ea2007c3bb
Rowan LeBlanc <role7830@colorado.edu>
2025-09-14 21:20:58 -0600

    Update main_Lab_2.m

--- main_Lab_2.m
+++ main_Lab_2.m
@@ -1,6 +1,6 @@
-clear;
-clear all;
-clc;
+clear
+clc
+close all
 
 [t_vec_1, av_pos_inert_1, av_att_1, tar_pos_inert_1, tar_att_1] = LoadASPENData('3801_Sec002_Test1.csv');
 [t_vec_2, av_pos_inert_2, av_att_2, tar_pos_inert_2, tar_att_2] = LoadASPENData('3801_Sec002_Test2.csv');
@@ -8,10 +8,13 @@ clc;
 
 %% Problem 3
 
-    figure(); 
-    hold on;
-    plot3(av_pos_inert_1(1,:), av_pos_inert_1(2,:), av_pos_inert_1(3,:));
-    plot3(tar_pos_inert_1(1,:),tar_pos_inert_1(2,:),tar_pos_inert_1(3,:));
+figure
+hold on
+grid on
+plot3(av_pos_inert_1(1,:), av_pos_inert_1(2,:), av_pos_inert_1(3,:))
+plot3(tar_pos_inert_1(1,:),tar_pos_inert_1(2,:),tar_pos_inert_1(3,:))
+legend('Vehicle', 'Target')
+xlabel('mm'), ylabel('mm'), zlabel('mm')
 
 %% Problem 4
 
@@ -53,35 +56,41 @@ for i = 1:3
 
 end
 
-
-
-
 %% Problem 5
 
 n1 = size(av_pos_inert_1,2);
 att313_av_1 = zeros(3,n1);
 att313_tar_1 = zeros(3,n1);
+
 for k = 1:n1
     DCM_av = RotationMatrix321(av_att_1(:,k));
     DCM_tar = RotationMatrix321(tar_att_1(:,k));
-    att313_av_1(:,k) = EulerAngles313_(DCM_av);
-    att313_tar_1(:,k) = EulerAngles313_(DCM_tar);
+    att313_av_1(:,k) = EulerAngles313(DCM_av);
+    att313_tar_1(:,k) = EulerAngles313(DCM_tar);
 end
 
-figure;
+yaxis = ['1', '2', '3'];
+
+figure
 for i = 1:3
     subplot(3,1,i)
-    plot(t_vec_1, rad2deg(att313_av_1(i,:)), '-b'); hold on;
-    plot(t_vec_1, rad2deg(att313_tar_1(i,:)), '--r');
-    ylabel(['313 angle ',num2str(i),' [deg]']);
-    if i==1, title('Problem 5: 3-1-3 Euler angles (Test 1)'); end
-    if i==3, xlabel('Time [s]'); end
-    legend('Vehicle','Target');
-    grid on;
+    hold on
+    plot(t_vec_1, rad2deg(att313_av_1(i,:)), '-b')
+    plot(t_vec_1, rad2deg(att313_tar_1(i,:)), '--r')
+    ylabel('Degrees')
+    if i==1
+        title('Problem 5: 3-1-3 Euler angles (Test 1)') 
+    end
+    if i==3
+        xlabel('Time (s)')
+    end
+    title('Euler Angle ', yaxis(i))
+    legend('Vehicle','Target')
+    grid on
 end
 
-
 %% Problem 6
+
 FROMavTOtar_vec_inert_1 = [tar_pos_inert_1(1,:)-av_pos_inert_1(1,:); tar_pos_inert_1(2,:)-av_pos_inert_1(2,:); tar_pos_inert_1(3,:)-av_pos_inert_1(3,:)];
 hold off
 
@@ -104,7 +113,9 @@ plot(t_vec_1,FROMavTOtar_vec_inert_1(3,:)/1000);
 title('z');
 xlabel('Time [s]');
 ylabel('Position [m]');
+
 %% Problem 7
+
 FROMavTOtar_vec_av_1=[];
 i=1;
 while i<size(av_att_1(1,:),2)+1
@@ -132,22 +143,21 @@ title('z');
 xlabel('Time [s]');
 ylabel('Position [m]');
 
-
 function DCM = RotationMatrix321(attitude321)
-
-%{
-
-Goal: Use the Euler angles for the 3-2-1 rotation sequence to calculate the associated DCM
-
-Inputs:
-• attitude321: 3 x 1 vector with the 3-2-1 Euler angles in the form attitude321 = [𝛼, 𝛽, 𝛾]T (In
-ASEN 3728 notation, this would be equivalent to [𝜙,𝜃,𝜓]T )
-
-Outputs:
-• DCM: the rotation matrix calculated from the Euler angles.
-
-%}
-
+% 
+% %{
+% 
+% Goal: Use the Euler angles for the 3-2-1 rotation sequence to calculate the associated DCM
+% 
+% Inputs:
+% • attitude321: 3 x 1 vector with the 3-2-1 Euler angles in the form attitude321 = [𝛼, 𝛽, 𝛾]T (In
+% ASEN 3728 notation, this would be equivalent to [𝜙,𝜃,𝜓]T )
+% 
+% Outputs:
+% • DCM: the rotation matrix calculated from the Euler angles.
+% 
+% %}
+% 
 a = attitude321(1); 
 b = attitude321(2);
 g = attitude321(3);
